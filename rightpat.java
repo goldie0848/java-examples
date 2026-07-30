1646df7817eabc23131e160c9dcbb78313fef9d7
Axcel <brazil0848@gmail.com>
2026-07-30 14:56:31 +0530

    Add files via upload

--- rightpat.java
+++ rightpat.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class sqpat {
+public class rightpat {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the size: ");
+        System.out.print("Enter size: ");
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
